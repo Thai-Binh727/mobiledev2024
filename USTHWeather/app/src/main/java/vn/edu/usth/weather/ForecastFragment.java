036d7b1d45345deed14441223c3cd68057f5a65d
Thai-Binh727 <binhnt0609@gmail.com>
2024-09-11 22:10:47 +0700

    Updated practical 5

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -16,28 +16,28 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
+        return inflater.inflate(R.layout.fragment_forecast, container, false);
 
         // Create linear layout with vertical orientaion
-        LinearLayout linear_layout = new LinearLayout(getActivity());
-        linear_layout.setOrientation(LinearLayout.VERTICAL);
-
-        // Create image view and add the image to that
-        ImageView imageView = new ImageView(getActivity());
-        imageView.setImageResource(R.drawable.weather_icon_1);
-        linear_layout.addView(imageView);
-
-        // Create text view and add text to that
-        TextView textView = new TextView(getActivity());
-        textView.setText("Thursday");
-        linear_layout.addView(textView);
-        textView.setTextColor(0xFF000000);
-
-        // Create a view group to add both text and image view to main
-        ViewGroup rootView = view.findViewById(R.id.main);
-        rootView.addView(linear_layout);
-
-        view.setBackgroundColor(0xCC7DDEEA);
-        return view;
+//        LinearLayout linear_layout = new LinearLayout(getActivity());
+//        linear_layout.setOrientation(LinearLayout.VERTICAL);
+//
+//        // Create image view and add the image to that
+//        ImageView imageView = new ImageView(getActivity());
+//        imageView.setImageResource(R.drawable.weather_icon_2);
+//        linear_layout.addView(imageView);
+//
+//        // Create text view and add text to that
+//        TextView textView = new TextView(getActivity());
+//        textView.setText("Thursday");
+//        linear_layout.addView(textView);
+//        textView.setTextColor(0xFF000000);
+//
+//        // Create a view group to add both text and image view to main
+//        ViewGroup rootView = view.findViewById(R.id.main);
+//        rootView.addView(linear_layout);
+//
+//        view.setBackgroundColor(0xFFFFFFFF);
+//        return view;
     }
 }
--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -18,12 +18,12 @@ public class WeatherActivity extends AppCompatActivity {
 
         // Practical 3
         setContentView(R.layout.fragment_forecast);
-        // Create a new Fragment to be placed in the activity layout
-        ForecastFragment firstFragment = new ForecastFragment();
-        // Add the fragment to the 'container' FrameLayout
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.main, firstFragment)
-                .commit();
+//        // Create a new Fragment to be placed in the activity layout
+//        ForecastFragment firstFragment = new ForecastFragment();
+//        // Add the fragment to the 'container' FrameLayout
+//        getSupportFragmentManager().beginTransaction()
+//                .add(R.id.main, firstFragment)
+//                .commit();
     }
 
     @Override
