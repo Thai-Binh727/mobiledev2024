fbf00b3300e2f6e215a45151489b76ab98211f2c
Thai-Binh727 <binhnt0609@gmail.com>
2024-09-11 10:48:15 +0700

    Updated practical 4

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -7,61 +7,37 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ForecastFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ForecastFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ForecastFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ForecastFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ForecastFragment newInstance(String param1, String param2) {
-        ForecastFragment fragment = new ForecastFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(0x2000FF00);
 
+        // Create linear layout with vertical orientaion
+        LinearLayout linear_layout = new LinearLayout(getActivity());
+        linear_layout.setOrientation(LinearLayout.VERTICAL);
+
+        // Create image view and add the image to that
+        ImageView imageView = new ImageView(getActivity());
+        imageView.setImageResource(R.drawable.weather_icon_1);
+        linear_layout.addView(imageView);
+
+        // Create text view and add text to that
+        TextView textView = new TextView(getActivity());
+        textView.setText("Thursday");
+        linear_layout.addView(textView);
+        textView.setTextColor(0xFF000000);
+
+        // Create a view group to add both text and image view to main
+        ViewGroup rootView = view.findViewById(R.id.main);
+        rootView.addView(linear_layout);
+
+        view.setBackgroundColor(0xCC7DDEEA);
         return view;
     }
 }
